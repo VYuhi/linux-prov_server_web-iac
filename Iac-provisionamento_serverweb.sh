8763b5c824fe176e10c6974eade5c16f49ffd50a
vyuhi <vyuhi.sugahara@outlook.com>
2023-02-24 20:30:58 +0000

    Apache v.1.0

---
+++ Iac-provisionamento_serverweb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "starting"
+
+#updating the server:
+
+apt-get update
+apt-get upgrade -y
+
+#installing apache2, unzip:
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+#Download the upload's file and unzip:
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+#copy files to /var/www/html/:
+
+cd  linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "end"
